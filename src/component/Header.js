9ed8c0f15d85e0143d0ee8fb2e2f01405a2f6d03
legha33-tech <deeplegha33@gmail.com>
2026-01-12 09:25:11 +0000

    header component added

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import "../Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h2>my website header</h2>
+      <p>this is header component</p>
+    </div>
+  );
+}
+
+export default Header;
